5450a62bb9910c9b0ebb35cbbbbcc6c8815509af
e174236m <giobucco99@gmail.com>
2019-11-15 00:25:12 +0100

    toto

---
+++ MiniProjetAndroid/app/src/main/java/com/example/miniprojetandroid/Accueil.java
@@ -0,0 +1,14 @@
+package com.example.miniprojetandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acticity_accueil);
+    }
+}
